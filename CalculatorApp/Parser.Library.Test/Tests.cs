9118731ec14c1456d04910bcf1711a15566aaa35
Matt Schnider <matt.schnider@gmail.com>
2019-01-27 21:25:48 -0500

    Parser now maps string operators to the appropriate function as demonstrated by the unit tests

---
+++ CalculatorApp/Parser.Library.Test/Tests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Parser.Library.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void Performs_Addition_When_Read_Plus()
+        {
+            int a = 3, b = 4;
+            double c = 3.9292, d = 19.3393;
+
+            int result = Parser.Operator("+",a,b);
+            double dResult = Parser.Operator("+", c, d);
+
+            Assert.AreEqual(result, a + b);
+            Assert.AreEqual(dResult, c + d);
+        }
+        [Test]
+        public void Performs_Subtraction_When_Read_Minus()
+        {
+            int a = 10, b = 5;
+            double c = 13.2930, d = 9.892;
+
+            int result = Parser.Operator("-", a, b);
+            double dResult = Parser.Operator("-", c, d);
+
+            Assert.AreEqual(result, a - b);
+            Assert.AreEqual(dResult, c - d);
+        }
+        [Test]
+        public void Performs_Mult_When_Read_Asterisk()
+        {
+            int a = 10, b = 12;
+            double c = 13.3222, d = 19.3333;
+
+            int result = Parser.Operator("*", a, b);
+            double dResult = Parser.Operator("*", c, d);
+        
+            Assert.AreEqual(result, a * b);
+            Assert.AreEqual(dResult, c * d);
+        }
+        [Test]
+        public void Performs_Div_When_Read_Slash()
+        {
+            int a = 14, b = 7;
+            double c = 13.3332, d = 3.2390;
+
+            int result = Parser.Operator("/", a , b);
+            double dResult = Parser.Operator("/", c, d);
+
+            Assert.AreEqual(result, a / b);
+            Assert.AreEqual(dResult, c / d);
+        }
+    }
+}
---
+++ CalculatorApp/Parser.Library/Parser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Parser.Library
+{
+    public class Parser
+    {
+       public static int Operator(string inputOperator, int a , int b)
+        {
+            int result = 0;
+            switch(inputOperator)
+            {
+                case "+":
+                    result = CalcFunctions.Library.CalcFunctions.Add(a, b);
+                    break;
+                case "-":
+                    result = CalcFunctions.Library.CalcFunctions.Subtract(a, b);
+                    break;
+                case "*":
+                    result = CalcFunctions.Library.CalcFunctions.Multiply(a, b);
+                    break;
+                case "/":
+                    result = CalcFunctions.Library.CalcFunctions.Divide(a, b);
+                    break;
+                default:
+                    break;
+
+            }
+            return result;
+        }
+        public static double Operator(string inputOperator, double a, double b)
+        {
+            double result = 0;
+            switch (inputOperator)
+            {
+                case "+":
+                    result = CalcFunctions.Library.CalcFunctions.Add(a, b);
+                    break;
+                case "-":
+                    result = CalcFunctions.Library.CalcFunctions.Subtract(a, b);
+                    break;
+                case "*":
+                    result = CalcFunctions.Library.CalcFunctions.Multiply(a, b);
+                    break;
+                case "/":
+                    result = CalcFunctions.Library.CalcFunctions.Divide(a, b);
+                    break;
+                default:
+                    break;
+
+            }
+            return result;
+        }
+    }
+}
